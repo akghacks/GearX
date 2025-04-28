5bc126b64f1a1bc8bedd671d33e5536e94dfdf25
BananaPrograms <chickennuggergaming@gmail.com>
2025-04-28 18:00:40 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,13 @@
+-- Made By Diddy
+print("im gay epic script")
+print("jkjkjk im kidding")
+-- Level
+printidentity()
+print("^ Executor level")
+-- UNC Test
+loadstring(game:HttpGet("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua", true))()
+-- sUNC Test
+loadstring(game:HttpGet("https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
+
+-- Bye Bye
+print("good or nah?")
