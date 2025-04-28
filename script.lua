9d1b8bd389fcd2953db2cbd817fe64f642e60aec
BananaPrograms <chickennuggergaming@gmail.com>
2025-04-28 18:07:44 -0400

    Update script.lua

--- script.lua
+++ script.lua
@@ -7,7 +7,7 @@ print("^ Executor level")
 -- UNC Test
 loadstring(game:HttpGet("https://raw.githubusercontent.com/unified-naming-convention/NamingStandard/main/UNCCheckEnv.lua", true))()
 -- sUNC Test
-loadstring(game:HttpGet("https://gitlab.com/sens3/nebunu/-/raw/main/HummingBird8's_sUNC_yes_i_moved_to_gitlab_because_my_github_acc_got_brickedd/sUNCm0m3n7.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/akghacks/GearX/refs/heads/main/script.lua"))()
 
 -- Bye Bye
 print("good or nah?")
